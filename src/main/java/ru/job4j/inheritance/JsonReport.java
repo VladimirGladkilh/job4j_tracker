ca1e6a4ac4b111669ed39574128dd4fd4e2e0106
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-18 14:47:00 +0500

    5. Аннотация @Override[#248702]

--- src/main/java/ru/job4j/inheritance/JsonReport.java
+++ src/main/java/ru/job4j/inheritance/JsonReport.java
@@ -1,5 +1,6 @@
 package ru.job4j.inheritance;
 public class JsonReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return "{\n"  +
                 "name:" + name + ",\n" +
