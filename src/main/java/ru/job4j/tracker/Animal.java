2e9f75ce6916287a21865d19979295492b05ca37
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-18 11:10:26 +0500

    2. Перегрузка конструктора.[#248700]

--- src/main/java/ru/job4j/tracker/Animal.java
+++ src/main/java/ru/job4j/tracker/Animal.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
 public class Animal {
+
+    public Animal(String name) {
+        super();
+        System.out.println("Create Animal"+ name);
+    }
     public Animal() {
         super();
         System.out.println("Create Animal");
