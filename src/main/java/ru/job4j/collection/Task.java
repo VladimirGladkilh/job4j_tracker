027de25ef4040e0b9576c76a80f710c5b594163e
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-31 16:19:11 +0500

    1. Уникальные задачи.[#248631]

--- src/main/java/ru/job4j/collection/Task.java
+++ src/main/java/ru/job4j/collection/Task.java
@@ -3,7 +3,17 @@ package ru.job4j.collection;
 public class Task {
     private String desc;
     private int priority;
+    private String number;
+    private String description;
 
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
     public Task(String desc, int priority) {
         this.desc = desc;
         this.priority = priority;
