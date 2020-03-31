656fa7a5eca83d6154e3f8121cdff418d52493ab
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-31 14:07:00 +0500

    2. Очередь с приоритетом на LinkedList[#248619]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -1,8 +1,6 @@
-package ru.job4j.pojo.search;
+package ru.job4j.search;
 
 import org.junit.Test;
-import ru.job4j.search.Person;
-import ru.job4j.search.PhoneDictionary;
 
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
