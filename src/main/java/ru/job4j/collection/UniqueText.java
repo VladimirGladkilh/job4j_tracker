1ff85074b0099a93cd97cc3f72812a237b921542
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-03-31 16:49:04 +0500

    2. Идентичные тексты.[#248632]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        // for-each origin -> new HashSet.
+        // for-each text -> hashSet.contains
+        for (String orig: origin) {
+            check.add(orig);
+        }
+        for (String tex: text) {
+            if (!check.contains(tex)) {
+                rsl = false;
+                break;
+            };
+        }
+        return rsl;
+    }
+}
