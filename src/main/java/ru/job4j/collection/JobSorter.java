70c474eb515aa220a0c2f211125f55681cfa03a4
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-04-20 16:06:59 +0500

    1. Collection API Улучшения[#248497]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -1,28 +1,37 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class JobSorter {
     public static void main(String[] args) {
-        List<Job> jobs = Arrays.asList(
+        /*List<Job> jobs = Arrays.asList(
                 new Job("Fix bugs", 4),
                 new Job("Impl task", 2),
                 new Job("Reboot server", 1)
-        );
+        );*/
+        List<Job> jobs = new ArrayList<>(
+                List.of(new Job("Fix bugs", 4),
+                new Job("Impl task", 2),
+                new Job("Reboot server", 1)));
         System.out.println(jobs);
         Collections.sort(jobs);
         System.out.println(jobs);
         Collections.sort(jobs, new SortByNameJob());
         System.out.println(jobs);
 
-        List<Job> jobs2 = Arrays.asList(
+        /*List<Job> jobs2 = Arrays.asList(
                 new Job("Fix bug", 1),
                 new Job("Fix bug", 4),
                 new Job("Fix bug", 2),
                 new Job("X task", 0)
+        );*/
+        List<Job> jobs2 = new ArrayList<>(
+                List.of(
+                        new Job("Fix bug", 1),
+                        new Job("Fix bug", 4),
+                        new Job("Fix bug", 2),
+                        new Job("X task", 0)
+                )
         );
         Collections.sort(jobs2, new JobDescByName().thenComparing(new JobDescByPriority()));
         System.out.println(jobs2);
