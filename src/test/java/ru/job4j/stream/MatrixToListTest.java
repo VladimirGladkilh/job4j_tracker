f6b657e92860748cb8c1e85b936c18b0346c9e22
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-04-21 09:43:41 +0500

    4. Преобразование List в Map.[#248493]

--- src/test/java/ru/job4j/stream/MatrixToListTest.java
+++ src/test/java/ru/job4j/stream/MatrixToListTest.java
@@ -10,26 +10,14 @@ import static org.junit.Assert.assertThat;
 public class MatrixToListTest {
     @Test
     public void Then3to3Test() {
-        Integer[][] intMatrix = new Integer[3][3];
-        for (int i = 0; i < 3; i ++) {
-            for (int j = 0; j < 3; j ++) {
-                intMatrix[i][j] = i * j;
-            }
-        }
-
+        Integer[][] intMatrix = {{0, 0, 0}, {0, 1, 2}, {0, 2, 4}};
         List<Integer> integerList = MatrixToList.convert(intMatrix);
         List<Integer> validList = List.of(0, 0, 0, 0, 1, 2, 0, 2, 4);
         assertThat(integerList.equals(validList), is(true));
     }
     @Test
     public void Then2to2Test() {
-        Integer[][] intMatrix = new Integer[2][2];
-        for (int i = 1; i < 3; i ++) {
-            for (int j = 1; j < 3; j ++) {
-                intMatrix[i-1][j-1] = i * j;
-            }
-        }
-
+        Integer[][] intMatrix = {{1, 2}, {2, 4}};
         List<Integer> integerList = MatrixToList.convert(intMatrix);
         List<Integer> validList = List.of(1, 2, 2, 4);
         assertThat(integerList.equals(validList), is(true));
