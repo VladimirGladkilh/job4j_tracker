dc22fbee3c1d681104e8a25c32fdb30a649db4d3
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-18 19:03:05 +0500

    1. Sql схема для проекта Tracker.[276987#291461]

---
+++ src/main/java/ru/job4j/db/insert.sql
@@ -0,0 +1,7 @@
+insert into items(name)  values('Some text 1');
+insert into items(name)  values('Some text 2');
+insert into items(name)  values('Some text 3');
+insert into items(name)  values('Some text 4');
+insert into items(name)  values('Some text 5');
+insert into items(name)  values('Some text 6');
+insert into items(name)  values('Some text 7');
